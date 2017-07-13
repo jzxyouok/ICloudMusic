84063c3488a1948f18e26f64974d7eb4e106000f
dph0105 <824394154@qq.com>
2017-07-13 15:36:01 +0800

    测试

--- dinglibrary/src/main/java/com/ding/god/dinglibrary/utils/ImageManager.java
+++ dinglibrary/src/main/java/com/ding/god/dinglibrary/utils/ImageManager.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.request.RequestOptions;
 
 public class ImageManager {
 
+    //hjahahah
     private Context mContext;
 
     private RequestOptions requestOptions = new RequestOptions()
